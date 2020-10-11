fd84bda03b40138c891a710f31f61d88aa2511cc
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-10-11 21:24:22 +0300

    Update travis config

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib calculator version Kubernetes</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other page</Link>
+          <Link to="/otherpage">Other page test</Link>
         </header>
         <Route exact path="/" component={Fib} />
         <Route exact path="/otherpage" component={OtherPage} />
